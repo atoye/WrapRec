cce31d53a5d098c27f9364a48d120e8ad21e8d8f
Babak Loni <babak.loni@gmail.com>
2015-03-04 14:32:09 +0100

    Introduced NLog to the project for logging

---
+++ RecommendationFramework2/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace WrapRec
+{
+    public static class Log
+    {
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+    }
+}
